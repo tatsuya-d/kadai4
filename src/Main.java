87542a45d1ef589ac85da01eee6a49f6952dff70
tatsuya <daidohtatsuya@Mac.local>
2023-05-25 00:22:15 +0900

    stream

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        var member = List.of("Mitoma","Minamino","Tomiyasu","Tanaka" );
+
+        var result = member.stream().filter(s -> s.contains("M")).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(result);
+
+    }
+}
